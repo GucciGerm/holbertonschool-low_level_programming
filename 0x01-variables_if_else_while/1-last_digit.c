fee84f64de46257bd806e371c0a22747453d6623
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-19 01:33:07 +0000

    replaced more content

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %d is %d greater than 5\n", n, x);
 	else if (x < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
-	else
+	else (x == 0)
 		printf("Last digit of %d is %d and is 0\n", n, x);
 	return (0);
 }
