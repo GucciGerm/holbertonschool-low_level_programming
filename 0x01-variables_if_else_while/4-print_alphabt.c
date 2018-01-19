a157013a7fa27fee2445c32438019a1c8df3879a
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-19 05:12:21 +0000

    Replaced more content in the 4th

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (!(ch == 'q' || ch == 'e'
-		      putchar (ch);
+		if (!(ch == 'q' || ch == 'e'))
+			putchar (ch);
 	}
 	putchar ('\n');
 	return (0);
