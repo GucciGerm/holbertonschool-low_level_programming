3bd1f76c168497ea753deff04f08bd28ebc81e53
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-23 21:43:52 +0000

    Add new content

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - This function checks for lowercased characters
+ *
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{ return (1);
+	}
+	{ return (0);
+	}
+
+}
