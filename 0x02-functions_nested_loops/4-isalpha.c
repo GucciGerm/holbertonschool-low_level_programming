de1cbeb5190b11082eebda4fe5c93bdf3f59ff05
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-23 22:12:52 +0000

    Added more content to the 4th

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - This function checks for alphabetic character
+ * @c: C is a letter that would output lowercase and uppercase
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{ return (1);
+	}
+	{ return (0);
+	}
+
+}
