bebd068dc702186004c0da1b9be4f0652a16c9d6
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-23 23:43:43 +0000

    Replaced more content within the 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,13 +4,12 @@
  * _abs - Function will compute the absolute value of an integer
  *@x: Random number
  *
+ * Return: x
  */
 
 int _abs(int x)
 {
 	if (x < 0)
-	{	x = (x * -1);
-		_putchar(x);
-	}
-	return (0);
+	x = (x * -1);
+	return (x);
 }
