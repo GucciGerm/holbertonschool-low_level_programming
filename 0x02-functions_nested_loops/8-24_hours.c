9a2881708876f4ab08815630347f0e1d7722c0fa
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-24 03:00:57 +0000

    Replaced more content

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  * jack_bauer - function that prints every minute of the day of Jack Bauer
  *
  *
- * Return: 0
+ *
  */
 
 void jack_bauer(void)
@@ -12,15 +12,17 @@ void jack_bauer(void)
 	int x;
 	int y;
 
-	for (x = 0; x < 23; x++)
+	for (x = 0; x <= 23; x++)
 	{
-	for (y = 0; y < 59; y++)
+	for (y = 0; y <= 59; y++)
 	{
 	_putchar(x / 10 + '0');
 	_putchar(x % 10 + '0');
 	_putchar(':');
 	_putchar(y / 10 + '0');
 	_putchar(y % 10 + '0');
+	_putchar('\n');
+
 	}
 	}
 }
