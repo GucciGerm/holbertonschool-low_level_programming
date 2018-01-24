246a95103cd2d88417969a2403d2846508523cbe
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-24 02:53:55 +0000

    Replace more content in the 8th

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 	int x;
 	int y;
 
-	for( x= 0; x < 23; x++)
+	for (x = 0; x < 23; x++)
 	{
-	for( y= 0; y < 59; y++)
+	for (y = 0; y < 59; y++)
 	_putchar(x / 10 + '0');
 	_putchar(x % 10 + '0');
 	_putchar(':');
