2d21069a3ca7283a517da95b5a78770cc0ccce69
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-24 00:20:09 +0000

    Replaced the return function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - This function prints the last digit of a number
+ *
+ * Return: x
+ */
+
+int print_last_digit(int x)
+{
+	x = x % 10;
+	if (x < 0)
+	{
+		x = (x * -1);
+		}
+	return (x);
+}
