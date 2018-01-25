5a7d7adc4ca471ff4abcbf03bdff8c140dd8f255
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-25 21:59:42 +0000

    Added more content to the second file

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Function multiplies two integers
+ * @a: first integer , @b: second integer
+ *
+ * Return: (a * b)
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
