ab8f31f69e819b18a7ca79433511d958ec607ad8
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-25 22:03:43 +0000

    Added b parameter

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,13 @@
 
 /**
  * mul - Function multiplies two integers
- * @a: first integer , @b: second integer
+ * @a: first integer
+ * @b: second integer
  *
  * Return: (a * b)
  */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
