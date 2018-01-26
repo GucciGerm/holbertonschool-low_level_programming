df1996c08e29e32c40bc1ba0117457a729cf010a
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-26 03:18:59 +0000

    Added equation in 6th file

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - Function will draw a straight line in the terminal
+ * @n - the number of times the _ will be printed
+ *
+ * Return: 0
+ */
+
+void print_line(int n);
+
+{
+	int a;
+
+	if(!(n <= 0))
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
