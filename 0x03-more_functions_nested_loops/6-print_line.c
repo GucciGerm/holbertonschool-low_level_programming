23af172698eddc74241adf3c849e1152010a838b
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-26 03:23:01 +0000

    Removed semi colon

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -2,12 +2,12 @@
 
 /**
  * print_line - Function will draw a straight line in the terminal
- * @n - the number of times the _ will be printed
+ * @n: the number of times the _ will be printed
  *
  * Return: 0
  */
 
-void print_line(int n);
+void print_line(int n)
 
 {
 	int a;
