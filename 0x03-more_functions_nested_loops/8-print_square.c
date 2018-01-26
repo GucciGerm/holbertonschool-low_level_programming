691a4ca448713a52788a61b77b6d61e844c7844c
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-26 04:45:57 +0000

    Added content to the 8th file

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_square - Function prints a square followed by a new line
+ * @size: represents the size of the square
+ *
+ * Return: 0
+ */
+
+void print_square(int size);
+
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
