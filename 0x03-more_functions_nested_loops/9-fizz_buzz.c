2ce606b6c9e5529bd0ad3ec2949cb140f215792e
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-26 05:40:04 +0000

    Trouble shooting betty

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,19 @@
  */
 
 int main(void)
-
 {
 	int a;
 
 	for (a = 1; a <= 100; a++)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
-			{
-				printf("FizzBuzz");
-			}
+		{
+			printf("FizzBuzz");
+		}
 		else if ((a % 3 == 0) && !(a % 5 == 0))
-				{
-					printf("Fizz");
-				}
+		{
+			printf("Fizz");
+		}
 		else if ((a % 5 == 0) && !(a % 3 == 0))
 		{
 			printf("Buzz");
