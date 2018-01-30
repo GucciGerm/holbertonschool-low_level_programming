b2e606093044bbb97776b50780d6bd4c91f50d1e
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-30 23:49:40 +0000

    Added content to the 4th file

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Function prints a string in reverse
+ * @s: String printed in reverse
+ *
+ */
+
+void print_rev(char *s);
+{
+	int a = 0;
+
+	while (s[a] != 0)
+		a++;
+	for (a--; a >= 0; a--)
+		_putchar(s[a]);
+	_putchar('\n');
+}
