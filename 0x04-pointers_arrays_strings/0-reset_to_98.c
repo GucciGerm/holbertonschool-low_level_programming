9d6f1a0be1e8960642a6b8bcf33fbd1869f95f7d
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-30 20:40:01 +0000

    Reset to 98

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Function takes a point to an int as parameter update value
+ * @n: int *n is a pointer
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
