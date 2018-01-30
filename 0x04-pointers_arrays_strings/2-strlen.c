0c6ab78ccb4e2effe8f9311ed1b5253ba9f8ddbf
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-30 22:13:34 +0000

    Added content to strlen file

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - This function returns the length of a string
+ * @*s: Our string
+ *
+ * Return: The length
+ */
+
+int _strlen(char *s);
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; s[a] != '\0'; ++a)
+		a++
+			return (b);
+}
