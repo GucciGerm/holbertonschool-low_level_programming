e31519026102f0749ff64ab7645108cd0121c1c9
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-30 22:33:38 +0000

    Add content

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - Function prints a string followed by new line to stdout
+ *
+ *
+ */
+
+void _puts(char *str);
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
