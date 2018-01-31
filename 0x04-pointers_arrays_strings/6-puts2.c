2cf910b221afbfb8819c4b1a530a02ea4182c883
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-31 02:21:35 +0000

    Added content to the 6th

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Function that prints 1 char out of 2 of a string
+ * @str: This is our string that we will be using
+ *
+ */
+
+void puts2(char *str);
+{
+	int a;
+
+	for (a = 0; str[a] != 0; a++)
+		if (x % 2 == 0)
+			_putchar(str[a]);
+	_putchar('\n');
+}
