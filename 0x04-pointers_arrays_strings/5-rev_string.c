30656e8bfe3e8edd1386744d1c1e2627687d1a44
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-01-31 02:07:26 +0000

    Reverse string

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - Function verses a string
+ * s: *s is the string we're pointing at
+ *
+ */
+
+void rev_string(char *s)
+{
+	int length = 0, x, temp;
+
+	while (s[length + 1] != 0)
+		length++;
+	for (x = 0; x <= length / 2; x++)
+	{
+		temp = s[x], s[x] = s[length - x];
+		s[length - x] = temp;
+	}
+}
