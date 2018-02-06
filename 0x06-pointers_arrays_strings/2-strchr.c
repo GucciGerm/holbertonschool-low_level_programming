003ae63ad074cf0f2a39b8ac632a170d470d2fdd
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-06 02:00:51 +0000

    Added for loop to determine the length of my string

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 /**
  * *_strchr - This function locates a character in a string
  * @s: Our string we are locating character from
- * @c: is our limitor
+ * @c: is our character
  *
  * Return: 0 or s+j
  */
@@ -11,11 +11,16 @@
 char *_strchr(char *s, char c)
 {
 	int j;
+	int d;
 
-	for (j = 0; j < c; j++)
+	for (d = 0; s[d] != '\0'; d++)
 	{
-		if (s[j] == c)
-			return (s + j);
+
+		for (j = 0; j < s[d]; j++)
+		{
+			if (s[j] == c)
+				return (s + j);
+		}
 	}
 	return (0);
 }
