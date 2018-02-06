adb97a31eca389259ce61b62ffdf5564bd88d846
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-06 21:44:17 +0000

    Got the length of a prefix substring

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strspn - This function gets the length of a prefixed substring
+ * @s: is our string we will be changing
+ * @accept: is our prefix characters
+ *
+ * Return: i
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int j;
+	int d;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (d = 0; accept[d] != '\0'; d++)
+		{
+			if (s[j] == accept[d])
+			{
+				i++;
+				break;
+			}
+			d++;
+		}
+	}
+	return (i);
+
+}
