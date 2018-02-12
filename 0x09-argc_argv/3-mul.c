b889307b5ea27477c20b6b6d9da11d7bf6464906
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-12 01:44:03 +0000

    Learned how to multiply two numbers betty check

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 3)
 	{
-		printf("You need two numbers\n");
+		printf("Error\n");
 		return (1);
 	}
 	else
@@ -25,7 +25,6 @@ int main(int argc, char *argv[])
 		mul = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", mul);
-	(void) argc;
 	return (0);
 	}
 }
