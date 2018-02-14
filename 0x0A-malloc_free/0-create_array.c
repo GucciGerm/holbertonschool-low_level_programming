0dbe963cf1fe07fb55434a2106d50e6be3783a04
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-14 23:20:42 +0000

    Learned how to create an array with a specific character

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - Here our function creates an array of chars w/specific char
+ * @size: is one our integers
+ * @c: character c will be what we will be changing
+ *
+ * Return: Null or 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *j;
+	unsigned int g;
+
+	j = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (0);
+
+	if (j == 0)
+		return (0);
+
+	for (g = 0; g < size; g++)
+		j[g] = c;
+
+	return (0);
+}
