a2172bfeead65975be97a6c5a600c4b7f81218c6
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-15 23:28:58 +0000

    Added the needed prototypes to holberton h

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -3,3 +3,5 @@ int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
