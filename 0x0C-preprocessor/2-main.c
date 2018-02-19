385c76558ead85be2646dbd31f8d11bc694bb30d
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-19 22:39:39 +0000

    Learned how to print the name of a file it was compiled from

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Here my function will print the name of the file it was complied from
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
