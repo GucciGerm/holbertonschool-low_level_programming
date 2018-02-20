e15868e622dc7d8c3e8c310d970a0cd1e1be6fd6
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-20 18:31:12 +0000

    I learned how to define a type dog_t for the new name

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -10,12 +10,13 @@
  * Return: 0
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-};
+	char *owner;
+} dog_t;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
