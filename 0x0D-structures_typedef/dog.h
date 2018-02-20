ef1879a7c40715e8325bc9c69400ab3221626abe
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-20 04:53:48 +0000

    I learned how to use a structure declaration using puppies

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef GokuBabyDogge
+#define GokuBabyDogge
+
+/**
+ * struct dog - Here our function will define new type struct dog with elements
+ * @name: is the name of the dogge
+ * @owner: is the name of the owner
+ * @age: is the age of the pupper
+ *
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
