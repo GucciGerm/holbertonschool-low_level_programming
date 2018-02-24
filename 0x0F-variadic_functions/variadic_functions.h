c1f01f3866a4c2a1fdb9b2906aaf0d5ce2a59da6
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-24 04:04:44 +0000

    I have added my putchar to my header files

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef sumof
 #define sumof
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
