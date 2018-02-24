b80cba2df57f706f22c5dd3b984b930fca7afe9d
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-24 17:53:26 +0000

    I learned how to create a fun that prints strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - Here our function will print strings w/ new line
+ * @separator: This is our commas
+ * @n: This is our limit we are setting also our # of assignments
+ *
+ * Return: void return
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list listo; /* we are creating a list called listo*/
+	unsigned int jg; /*will be our count*/
+	char *db;
+
+	va_start(listo, n); /*we are starting up our list*/
+	if (separator == NULL)
+		return;
+
+	for (jg = 0; jg < n; jg++)
+	{
+		db = va_arg(listo, char *);
+		if (db == NULL)
+		{
+			printf("(nil)\n");
+		}
+		printf("%s", db);
+		if (jg < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n"); /*print new line*/
+	va_end(listo); /*ending our list*/
+}
