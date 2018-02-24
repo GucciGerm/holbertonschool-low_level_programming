176d62d6bfd35bb708253f84083af7dec8fd4b57
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-24 21:44:08 +0000

    Making changes to 2-print

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -17,20 +17,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *db;
 
 	va_start(listo, n); /*we are starting up our list*/
-	if (separator == NULL)
-		printf("%s", separator);
 
 	for (jg = 0; jg < n; jg++)
 	{
 		db = va_arg(listo, char *);
+		if (separator != NULL && jg > 0)
+			printf("%s", separator);
 		if (db == NULL)
 		{
 			printf("(nil)\n");
 		}
 		else
 			printf("%s", db);
-		if (jg < n - 1)
-			printf("%s", separator);
 	}
 	printf("\n"); /*print new line*/
 	va_end(listo); /*ending our list*/
