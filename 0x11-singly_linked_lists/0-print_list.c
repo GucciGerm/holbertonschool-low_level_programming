965752b3afc74c905d0c1fb50e48019687604916
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-03-02 01:40:57 +0000

    I learned to return the num of elements in a linked list

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Here our function will print all the elements of our list_t list
+ * @h: H will be representing our linked list variable
+ *
+ * Return: We will be returning our number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr = h;
+	size_t g;
+
+	for (g = 0; ptr != NULL; g++)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+	}
+	return (g);
+}
