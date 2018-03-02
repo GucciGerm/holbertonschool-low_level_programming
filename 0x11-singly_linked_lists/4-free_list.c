14dacac9a4beed45c12e3f93e1cea92ed6832185
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-03-02 21:48:43 +0000

    I learned how to free a list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+#include "string.h"
+
+/**
+ * free_list - Here our function is freeing our "list_t" list
+ * @head: Head is our "list_t"
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *placeholder = head;
+	while(head != NULL)
+	{
+		placeholder = head->next;
+		free(head->str);
+		free(head);
+		head = placeholder;
+	}
+}
