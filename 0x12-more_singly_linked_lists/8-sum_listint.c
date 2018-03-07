4199bdb7e07cdf7b81c0013f94036ee18cbc1758
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-03-07 04:06:11 +0000

    I removed unneeded edge

--- 0x12-more_singly_linked_lists/8-sum_listint.c
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -11,13 +11,8 @@
 
 int sum_listint(listint_t *head)
 {
-	int sum;
-
-	if (head == NULL)
-	{
-		return (0);
-	}
-	while (head != NULL)
+	int sum = 0;
+	while (head != NULL) 
 	{
 		sum += head->n;
 		head = head->next;
