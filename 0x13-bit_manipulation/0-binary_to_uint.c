625b6448104cf651b734568b17f9ffbf3b5a0dcb
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-03-09 01:32:49 +0000

    I learned how to convert from binary to unsigned int

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - Here our function converts a binary num to an unsigned int
+ * @b: is a pointer to a string of zer0 or 0ne
+ *
+ * Return: 0ne, Zer0, or NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int j;
+
+	for (j = 0; *b; b++)
+	{
+		if (*b == '1')
+			j = (j << 1) | 1;
+		else if (*b == '0')
+			j <<= 1;
+		else
+			return (0); /* because we want to return 0*/
+	}
+	return (j);
+}
