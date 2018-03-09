816d817b63db11e5466287e3570a6de663e8b66e
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-03-09 07:42:34 +0000

    made betty changes to comments!

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * get_bit - Here our function will return a bit at an given index
@@ -12,11 +11,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	// This will work specifically for 64 bit machine (: //
+	/* This will work specifically for 64 bit machine (: */
 	if (index > 63) /* index can go from 0-63 which is 64, account for 0 */
 		return (-1); /* return -1 if it's larger than a 64 bit*/
 	return ((n >> index) & 1); /*shift right to get our bit of index */
 
-	// & 1 means to checks each place to see if its 1 or 0 //
-	// using & 1 converts it into bits and not decimal //
+	/* & 1 means to checks each place to see if its 1 or 0 */
+	/* using & 1 converts it into bits and not decimal */
 }
