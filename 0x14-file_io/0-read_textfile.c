22068d2b46fe4de0c9a03d1920b1dea8c50e118f
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-03-14 00:30:10 +0000

    I learned how to print and write a file using I/O

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - Here our function will read a text file and print posix
+ * @letters: letters is the number of letters that should be read and print
+ * @filename: filename is our pointer to our "filename"
+ *
+ * Return: actual number of letters or zer0 or -1
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t length; /*for assigning our total length*/
+
+	/* mallocating space */
+	buffer = malloc(sizeof(char) * letters + 1);
+
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	if (filename == NULL)
+		return (0);
+	/* for reading */
+	fd = open(filename, O_RDONLY, 0400);
+
+	if (fd == -1)
+		return (0);
+
+	length = read(fd, buffer, letters); /*use buffer 2 count letters*/
+	buffer[letters] = '\0';
+
+
+	/* for writing */
+
+
+	fd = write(STDOUT_FILENO, buffer, length);
+	{
+		free(buffer);
+		close(fd);
+		return (length);
+	}
+
+	free(buffer);
+	close(fd);
+	return (length);
+}
