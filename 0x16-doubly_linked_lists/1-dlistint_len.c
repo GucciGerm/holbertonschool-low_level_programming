6c3d899ada02ac6b747bea910f9aef2db205865f
[GucciGerm] <germaine.balmania@gmail.com>
2018-04-14 01:17:53 +0000

    I learned how to print a dll and return the num of elements

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * dlistint_len - Here will return the number of elements in the linked list
+ * @h: is our doubly linked list variable
+ *
+ *
+ * Return: The number of elements within the dlistint_t list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elementcount = 0;
+	dlistint_t *doublylinklist = NULL;
+
+	doublylinklist = h->next;
+	elementcount++;
+
+	while (doublylinklist)
+	{
+		elementcount++;
+		doublylinklist = doublylinklist->next;
+		/* setting the dll to the next node */
+	}
+	return (elementcount);
+}
