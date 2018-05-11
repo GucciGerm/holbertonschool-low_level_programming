676241f48965a56fe48a66d0c7e21847e316e8bd
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-11 20:24:49 +0000

    accounting for if func went wrong

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -16,22 +16,25 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hashtable;
 	unsigned long int counter;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	hasharray = malloc(size * (sizeof(hash_node_t)));
 	if (hasharray == NULL)
 	{
-		free(hasharray);
 		return (NULL);
 	}
 	hashtable = malloc(sizeof(hash_table_t));
 	if (hashtable == NULL)
 	{
-		free(hashtable);
 		return (NULL);
 	}
 	for (counter = 0; counter < size; counter++)
 	{
 		hasharray[counter] = NULL;
 	}
+	free(hasharray);
 	hashtable->size = size;
 	hashtable->array = hasharray;
 
