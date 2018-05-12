d507f0532804689e644687d404e23ef20b10d893
[GucciGerm] <germaine.balmania@gmail.com>
2018-05-12 02:35:57 +0000

    added missing , within my printf statement

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * hash_table_print - This function will print a hashtable
+ * @ht: ht is the hashtable
+ *
+ * Return: print the key/val in order they appear in the array of hashtable
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+
+	unsigned long int idx = 0; /* start from the order they appear */
+	hash_node_t *hashtable;
+	int numofelements = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (idx < ht->size)
+	{
+		hashtable = ht->array[idx];
+		while (hashtable != NULL)
+		{
+			if (numofelements == 0)
+			{
+				printf("'%s': '%s'", hashtable->key,
+				      hashtable->value);
+				/* after you print you have now incremented*/
+				numofelements = 1;
+			}
+			else
+				/* account for what's inside */
+				printf(",'%s': '%s'", hashtable->key,
+				       hashtable->value);
+			hashtable = hashtable->next;
+
+		}
+		idx++; /* count through the hash table using index*/
+	}
+	printf("}\n");
+}
