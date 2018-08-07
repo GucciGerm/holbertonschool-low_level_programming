ba32e8bb454385f8bb66d59756d3c26b170c1d59
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-07 11:02:32 -0700

    adding header

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef algos
+#define algos
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * This is the header file
+ *
+ *
+ *
+ *
+ */
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
