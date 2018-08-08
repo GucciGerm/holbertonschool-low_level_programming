84e47b1c87ed8a949114996ca60c986a44b56978
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-08 09:42:49 -0700

    Removed comments from header file, made throwing betty error

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -4,14 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * This is the header file
- *
- *
- *
- *
- */
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
